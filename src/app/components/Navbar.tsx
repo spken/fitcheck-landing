97928fd4ec78f9bd7dab5ca55494062a721122b4
spken <varematias11@gmail.com>
2026-02-17 19:16:31 +0100

    remove navbar

--- src/app/components/Navbar.tsx
+++
@@ -1,7 +0,0 @@
-export default function Navbar() {
-  return (
-    <header className="px-6 py-6 max-w-6xl mx-auto">
-      <span className="text-xs tracking-wide text-[#8E8E8E]">FITCHECK</span>
-    </header>
-  );
-}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Narrative from "./components/Narrative";
 import FinalCTA from "./components/FinalCTA";
@@ -7,7 +6,6 @@ import Footer from "./components/Footer";
 export default function Home() {
   return (
     <main className="min-h-screen bg-[#0F0F0F] text-[#FAFAFA] font-sans">
-      <Navbar />
       <Hero />
       <Narrative />
       <FinalCTA />
